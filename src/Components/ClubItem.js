f01d7271191a5eb6e5a5eeb10b1a040151cd98fb
Reda BELHAJ <reda.belhaajj@gmail.com>
2022-01-16 21:57:37 +0100

    List Clubs and List Events

---
+++ src/Components/ClubItem.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import InfoIcon from '@mui/icons-material/Info';
+import EmailIcon from '@mui/icons-material/Email';
+
+const ClubItem = ({club, profileImg, coverImg, detail}) => {
+    return (
+        <div className="w-auto lg:m-3 mb-2 text-gray-800 bg-white shadow-md border-2 border-gray-200">
+            <div className="h-20" style={{backgroundImage: `url(${coverImg})`}}>
+                <div className="flex justify-center">
+                    <img 
+                        className="mt-8 object-cover w-24 h-24 mt-4 border-4 border-blue-500 rounded-full" 
+                        src={profileImg}
+                    />
+                </div>
+            </div>
+            <div className="px-6 py-2">
+                <div className="flex justify-center mt-10 mb-4 text-xl font-medium">{club}</div>
+                <div className="flex w-full text-gray-600">
+                    <InfoIcon className="h-5 mt-1 mr-2"/>
+                    <span>{detail}</span>
+                </div>
+
+                <div className="flex my-1 text-gray-600">
+                    <EmailIcon className="h-5 mt-1 mr-2"/>
+                    <span>email@email.com</span>
+                </div>
+            </div>
+            <div className="flex justify-center my-2">
+                <button 
+                    type="button" 
+                    className="inline-flex items-center px-6 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-500 rounded-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                >
+                    View Profile
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ClubItem
--- src/Pages/Clubs.js
+++ src/Pages/Clubs.js
@@ -1,48 +1,15 @@
-import React, {useState} from 'react'
-
-import ModalJoin from '../Components/ModalJoin';
+import React from 'react'
 
 import NavbarAuth from '../Components/NavbarAuth'
+import ListClubs from '../Components/ListClubs'
 
 const Clubs = () => {
-    const [showModal, setShowModal] = useState(false);
-
-    const handleOpen = () => setShowModal(true);
-    const handleClose = () => setShowModal(false);
-
     return (
         <div className='flex flex-col min-h-screen overflow-hidden'>
             <NavbarAuth />
-            <main>
-            <br></br><br></br> <br></br>
-                <div className="w-full lg:grid lg:grid-cols-7 gap-2 max-w-6xl mx-auto px-5 sm:px-6">
-                    <button 
-                            className="py-2 px-4 bg-blue-500 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                        >
-                            Follow
-                        </button>
-                </div>
-                <br></br><br></br> <br></br>
-                <div className="w-full lg:grid lg:grid-cols-7 gap-2 max-w-6xl mx-auto px-5 sm:px-6">
-                    <button
-                            onClick={handleOpen} 
-                            className="py-2 px-4 bg-blue-500 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                        >
-                            Join
-                    </button>
-                </div>
-                <br></br><br></br> <br></br>
-                <div className="w-full lg:grid lg:grid-cols-7 gap-2 max-w-6xl mx-auto px-5 sm:px-6">
-                    <h1 style={{whiteSpace: 'nowrap'}}><b>Clubs of which you are an Owner</b></h1>
-                </div>
-                <br></br><br></br> <br></br>
-                <div className="w-full lg:grid lg:grid-cols-7 gap-2 max-w-6xl mx-auto px-5 sm:px-6">
-                    <h1 style={{whiteSpace: 'nowrap'}}> <b>Clubs of which you are a member</b></h1>
-                </div>
-
-
+            <main className='mt-20 w-full max-w-6xl mx-auto px-5 sm:px-6'>
+                <ListClubs />
             </main>
-            <ModalJoin handleClose={handleClose} show={showModal}/>
         </div>
     )
 }
